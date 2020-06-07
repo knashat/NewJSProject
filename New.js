3b001c101bfc1552e808eba48b886641ee1931a8
knashat <65470904+knashat@users.noreply.github.com>
2020-06-07 23:25:34 +0200

    New-assignment

--- New.js
+++ New.js
@@ -8,27 +8,56 @@ const products = [
     name: "coffee",
     quantity: 20,
     type: "cups"
+  },
+
+  {
+    name: "tomato",
+    quantity: 30,
+    type: "Kilo"
   }
 ];
 
 function getProducts(a) {
-  console.log(a);
+  const productCopy = [...products];
+  console.log(productCopy);
 }
-getProducts(products);
+getProducts();
 
-function addNewProduct(NewProduct, NewQuantity, NewType) {
+function valString(text) {
+  if (typeof text !== "string") {
+    throw new Error(`"${text}" is not a valid string!`);
+  }
+}
+
+function valNumber(number) {
+  if (typeof number !== "number") {
+    throw new Error(`"${number}" is not a valid number!`);
+  }
+}
+
+function addNewProduct(newProduct, newQuantity, newType) {
+  valString(newProduct);
+  valString(newType);
+  valNumber(newQuantity);
   products.push({
-    name: NewProduct,
-    quantity: NewQuantity,
-    type: NewType
+    name: newProduct,
+    quantity: newQuantity,
+    type: newType
   });
   console.log(products);
 }
 
-addNewProduct("Chocolate", 10, "bars");
+addNewProduct("chocolate", 10, "bars");
 
-function UpdateQuantity(ProductIndex, NQuantity) {
-  products[ProductIndex].quantity = NQuantity;
-  console.log(products);
+function UpdateQuantity(productName, nQuantity) {
+  valString(productName);
+  valNumber(nQuantity);
+  let i;
+  for (i = 0; i < products.length; i++) {
+    if (products[i].name === productName) {
+      products[i].quantity = nQuantity;
+      return console.log(products);
+    }
+  }
 }
-UpdateQuantity(0, 17);
+UpdateQuantity("tomato", 45);
